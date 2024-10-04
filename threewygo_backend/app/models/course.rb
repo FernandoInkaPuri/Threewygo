9ef69eae8efe7f52e57bc6927429726d3c0a3df6
Luiz Fernando <luizfegomez@gmail.com>
2024-10-04 15:30:27 -0300

    Add routes and Course model validations

--- threewygo_backend/app/models/course.rb
+++ threewygo_backend/app/models/course.rb
@@ -1,2 +1,14 @@
 class Course < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :video_urls, presence: true
+  validate :end_date_greater_than_start_date
+
+  private
+  
+  def end_date_greater_than_start_date
+    errors.add(:end_date, 'Data de término precisa ser maior que data de início') if start_date > end_date
+  end
 end
