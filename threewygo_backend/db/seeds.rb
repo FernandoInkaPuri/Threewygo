b6b191b084c7d07dae47cbd60b8d89aa7920d67c
Luiz Fernando <luizfegomez@gmail.com>
2024-10-06 16:44:32 -0300

    Add Seeds

--- threewygo_backend/db/seeds.rb
+++ threewygo_backend/db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+courses = [
+  {
+    title: 'Curso de Dublagem',
+    description: 'Aprenda com o melhor da dublagem brasileira',
+    video_urls: ['https://www.youtube.com/watch?v=TCkWpRBAnzw'].to_json
+  },
+  {
+    title: 'Curso de Reiki',
+    description: 'Aprenda esta técnica maravilhosa de equilibrio e saúde',
+    video_urls: ['https://www.youtube.com/watch?v=NPpqSfgDJYU&list=PLBDxU1-FpooiBhsSzMe1_tr9clULY9EXu',
+    'https://www.youtube.com/watch?v=VXTHr4Glx3A&list=PLBDxU1-FpooiBhsSzMe1_tr9clULY9EXu&index=2&pp=iAQB',
+    'https://www.youtube.com/watch?v=EYOWuTlr948&list=PLBDxU1-FpooiBhsSzMe1_tr9clULY9EXu&index=3&pp=iAQB'].to_json
+  },
+  {
+    title: 'Como torcer pro corinthians',
+    description: 'E se tornar um torcedor apaixonado ',
+    video_urls: ['https://www.youtube.com/watch?v=DFt3mHpLg58',
+    'https://www.youtube.com/watch?v=IbeD6YarPvg&pp=ygUQY29yaW50aGlhbnMgMjAxMg%3D%3D'].to_json
+  }
+]
+
+courses.each do |course|
+  course[:start_date] = DateTime.now
+  course[:end_date] = DateTime.now + 222
+
+  Course.create(course)
+end
+
+puts "Seeds criado com sucesso!"
